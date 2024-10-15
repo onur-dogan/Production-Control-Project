f9a5ef36b3f82341b52e452de18b9e84b8b3f3c4
onur-dogan <aindres@hotmail.com>
2024-10-15 13:37:24 +0300

    feat(aircrafts): Implement aircraft windows and API features - Create a HTML file to show aircraft processes to the user on the UI - Create API functionalities for the aircraft processes - Use datatable on the UI to render the aircraft and production lists

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from .views.home import main as home
 from .views.login import Login
+from .views.signup import SignUp
+from .views.parts import Parts, PartListView
+from .views.aircrafts import Aircrafts, AircraftListView, AircraftProductionListView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', Login.as_view(), name='login'),
-    path('home/', home, name='home')
+    path('signup/', SignUp.as_view(), name='signup'),
+    path('parts/', Parts.as_view(), name='parts'),
+    path('partsData/', PartListView.as_view(), name='partsData'),
+    path('aircrafts/', Aircrafts.as_view(), name='aircrafts'),
+    path('aircraftsData/', AircraftListView.as_view(), name='aircraftsData'),
+    path('aircraftProductionsData/', AircraftProductionListView.as_view(), name='aircraftProductionsData'),
 ]
